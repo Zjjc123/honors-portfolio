f55fe01a1e52788f9424e3bf76d97cec988b3928
Jason Zhang <zjjc123@hotmail.com>
2022-01-06 15:58:34 -0800

    Move out Footer

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import React from "react";
 import styles from "../styles/Home.module.css";
+import { Footer } from "./Footer";
 import { Navbar } from "./Navbar";
 
 const Layout: NextPage = (props) => {
@@ -15,18 +15,7 @@ const Layout: NextPage = (props) => {
       </Head>
       <Navbar />
       {props.children}
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <Footer />
     </div>
   );
 };
